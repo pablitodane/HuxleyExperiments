738bcc515a411d270eb8e1ffb6309da4fa7cd4f9
Paolo Danesini <paolo.danesini@gmx.net>
2022-12-05 12:16:15 +0100

    Started changing the kinematic_model_input file with multiple choices

--- src/kinematic_model_input.m
+++ src/kinematic_model_input.m
@@ -1,16 +1,39 @@
 function [stim,lmtc,lmtcd]=kinematic_model_input(t,parms)
-%% Original code - Stimulation
-% if t<=.1
-%     stim=parms.gamma0; % we start in steady state
-% elseif t>.1 && t<2 % full activation
-%     stim=1; 
-% else
-%     stim=0.2; % relaxation to low value
-% end
-% lmtc0=parms.lmtc0;
-% lceopt=parms.lceopt;
+% This function can be modified to choose what kind of figure we want to have as an
+% output. The options are:
+% 1  - Original code from the tutorial
+% 2 - Simulate the figure 4a
+% 3 - Simulate the figure 4b
+% 4 - Simulate the figure 4c
+% 5 - Simulate the figure 4d
 
-%% Modified code - Stimulation
+what_code = 5;
+
+if what_code == 1
+    
+% Original code - Stimulation
+    if t<=.1
+        stim=parms.gamma0; % we start in steady state
+    elseif t>.1 && t<2 % full activation
+        stim=1; 
+    else
+        stim=0.2; % relaxation to low value
+    end
+lmtc0=parms.lmtc0;
+lceopt=parms.lceopt;
+
+% Original code - Kinematic input
+if t<=0.3
+    lmtc=lmtc0;
+    lmtcd=0;
+else
+    lmtc=lmtc0+0.2*lceopt*sin(2*pi*t);
+    lmtcd=0.2*2*pi*lceopt*cos(2*pi*t);
+end
+
+elseif what_code == 5;
+
+% Figure 4d - Modified code - Stimulation
 if t<=0.01
     stim=parms.gamma0; % we start in steady state
 elseif t>0.01 && t<0.505 % full activation
@@ -24,17 +47,7 @@ lceopt=parms.lceopt;
 % mtc length
 % isometric at lmtc0
 
-%% Original code - Kinematic input
-% if t<=0.3
-%     lmtc=lmtc0;
-%     lmtcd=0;
-% else
-% 
-% lmtc=lmtc0+0.2*lceopt*sin(2*pi*t);
-% lmtcd=0.2*2*pi*lceopt*cos(2*pi*t);
-% end
-
-%% Modified code - Kinematic input
+% Figure 4d - Modified code - Kinematic input
 if t<=0.49026
     lmtc=lmtc0;
     lmtcd=0;
@@ -45,5 +58,7 @@ else
     lmtc=lmtc0 + 0.002475*0.49026 - 0.002475*0.8943;
     lmtcd=0;
 end
+
+end % end-if
 
 end
