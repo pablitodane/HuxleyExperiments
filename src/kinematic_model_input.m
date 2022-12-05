9554ac1ba6bdb5ab0943072902a1d3ebdcd76fc6
Paolo Danesini <paolo.danesini@gmx.net>
2022-12-05 12:42:22 +0100

    Files ready to reproduce figures 2, 4a, 4b, 4c and 4d in paper

--- src/kinematic_model_input.m
+++ src/kinematic_model_input.m
@@ -7,7 +7,7 @@ function [stim,lmtc,lmtcd]=kinematic_model_input(t,parms)
 % 4 - Simulate the figure 4c
 % 5 - Simulate the figure 4d
 
-what_code = 5;
+what_code = 4;
 
 if what_code == 1
     
@@ -23,41 +23,128 @@ lmtc0=parms.lmtc0;
 lceopt=parms.lceopt;
 
 % Original code - Kinematic input
-if t<=0.3
-    lmtc=lmtc0;
-    lmtcd=0;
-else
-    lmtc=lmtc0+0.2*lceopt*sin(2*pi*t);
-    lmtcd=0.2*2*pi*lceopt*cos(2*pi*t);
-end
+    if t<=0.3
+        lmtc=lmtc0;
+        lmtcd=0;
+    else
+        lmtc=lmtc0+0.2*lceopt*sin(2*pi*t);
+        lmtcd=0.2*2*pi*lceopt*cos(2*pi*t);
+    end
 
-elseif what_code == 5;
 
-% Figure 4d - Modified code - Stimulation
-if t<=0.01
-    stim=parms.gamma0; % we start in steady state
-elseif t>0.01 && t<0.505 % full activation
-    stim=1;
-else
-    stim=0.0; % relaxation to zero
-end
+elseif what_code == 2
+
+% Figure 4a - Modified code - Stimulation
+    if t<=0.01
+        stim=parms.gamma0; % we start in steady state
+    elseif t>0.01 && t<0.5 % full activation
+        stim=1; 
+    else
+        stim=0.0; % relaxation to zero
+    end
+lmtc0=parms.lmtc0;
+lceopt=parms.lceopt;    
+
+% Figure 4a - Modified code - Kinematic input
+    if t<=0.47
+        lmtc=lmtc0;
+        lmtcd=0;
+    elseif t>0.47 && t<=0.48
+        lmtc=lmtc0 + 0.01*0.47 - 0.01*t;
+        lmtcd=-0.01;
+    elseif t>0.48 && t<=0.83
+        lmtc=lmtc0 + 0.01*0.47 - 0.01*0.48;
+        lmtcd=0;
+    elseif t>0.83 && t<=0.85
+        lmtc=lmtc0 + 0.01*0.47 - 0.01*0.48 - 0.005*0.83 + 0.005*t;
+        lmtcd=0.005;
+    else
+        lmtc=lmtc0 + 0.01*0.47 - 0.01*0.48 - 0.005*0.83 + 0.005*0.85;
+        lmtcd=0;
+    end
+
+
+elseif what_code == 3
+
+% Figure 4b - Modified code - Stimulation
+    if t<=0.01
+        stim=parms.gamma0; % we start in steady state
+    elseif t>0.01 && t<0.657 % full activation
+        stim=1;
+    else
+        stim=0.0; % relaxation to zero
+    end
 lmtc0=parms.lmtc0;
 lceopt=parms.lceopt;
 
-% mtc length
-% isometric at lmtc0
+% Figure 4b - Modified code - Kinematic input
+    if t<=0.497
+        lmtc=lmtc0;
+        lmtcd=0;
+    elseif t>0.497 && t<=0.504
+        lmtc=lmtc0 + 0.0143*0.497 - 0.0143*t;
+        lmtcd=-0.0143;
+    elseif t>0.504 && t<=1.002
+        lmtc=lmtc0 + 0.0143*0.497 - 0.0143*0.504 + 0.00161*0.504 -0.00161*t;
+        lmtcd=-0.00161;
+    else
+        lmtc=lmtc0 + 0.0143*0.497 - 0.0143*0.504 + 0.00161*0.504 -0.00161*1.002;
+        lmtcd=0;
+    end
+
+
+elseif what_code == 4
+
+% Figure 4c - Modified code - Stimulation
+    if t<=0.01
+        stim=parms.gamma0; % we start in steady state
+    elseif t>0.01 && t<0.85 % full activation
+        stim=1; 
+    else
+        stim=0.0; % relaxation to zero
+    end
+lmtc0=parms.lmtc0;
+lceopt=parms.lceopt;
+
+% Figure 4c - Modified code - Kinematic input
+    if t<=0.45
+        lmtc=lmtc0;
+        lmtcd=0;
+    elseif t>0.45 && t<=0.95
+        lmtc=lmtc0 - 0.001*0.45 + 0.001*t;
+        lmtcd=0.001;
+    else
+        lmtc=lmtc0 - 0.001*0.45 + 0.001*0.95;
+        lmtcd=0;
+    end
+
+
+
+
+elseif what_code == 5
+
+% Figure 4d - Modified code - Stimulation
+    if t<=0.01
+        stim=parms.gamma0; % we start in steady state
+    elseif t>0.01 && t<0.505 % full activation
+        stim=1;
+    else
+        stim=0.0; % relaxation to zero
+    end
+lmtc0=parms.lmtc0;
+lceopt=parms.lceopt;
 
 % Figure 4d - Modified code - Kinematic input
-if t<=0.49026
-    lmtc=lmtc0;
-    lmtcd=0;
-elseif t>0.49026 && t<=0.8943
-    lmtc=lmtc0 + 0.002475*0.49026 - 0.002475*t;
-    lmtcd=-0.002475;
-else
-    lmtc=lmtc0 + 0.002475*0.49026 - 0.002475*0.8943;
-    lmtcd=0;
-end
+    if t<=0.49026
+        lmtc=lmtc0;
+        lmtcd=0;
+    elseif t>0.49026 && t<=0.8943
+        lmtc=lmtc0 + 0.002475*0.49026 - 0.002475*t;
+        lmtcd=-0.002475;
+    else
+        lmtc=lmtc0 + 0.002475*0.49026 - 0.002475*0.8943;
+        lmtcd=0;
+    end
 
 end % end-if
 
